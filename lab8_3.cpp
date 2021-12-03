e8a6744a6a67f55858083ed96fea14fbf4dfd373
nop175 <tananop_them@gmail.com>
2021-12-03 16:35:57 +0700

    Q3

--- lab8_3.cpp
+++ lab8_3.cpp
@@ -1,19 +1,29 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-char before(char x){
-	//Write your function definition here
-}
 
+char before(char x){
+    char y= '0';
+    if(64<x&&x<91){
+        if(x>65){
+            return x-1;
+        }
+        if (x==65){
+            return x+25;
+        }
+
+    }
+    return y;
+
+}
 int main(){
-	//Test Case
-	cout << before('A') << "\n";
-	cout << before('B') << "\n";
-	cout << before('P') << "\n";
-	cout << before('T') << "\n";
-	cout << before('Z') << "\n";
-	cout << before('a') << "\n";
-	cout << before('0') << "\n";
-	cout << before('c') << "\n";
-	return 0;
+    cout << before('A') << "\n";
+    cout << before('B') << "\n";
+    cout << before('P') << "\n";
+    cout << before('T') << "\n";
+    cout << before('Z') << "\n";
+    cout << before('a') << "\n";
+    cout << before('0') << "\n";
+    cout << before('c') << "\n";
+    return 0;
 }
